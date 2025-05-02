4b301194d54dea656cbaa27a8bb6eafd331f0a73
hariharan-0 <100hariharan@gmail.com>
2025-05-03 00:02:55 +0530

    Authored Create
    Signed-off-by: hariharan-0 <100hariharan@gmail.com>

---
+++ src/app/Component/auro-editor/auro-editor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auro-editor',
+  imports: [],
+  templateUrl: './auro-editor.component.html',
+  styleUrl: './auro-editor.component.css'
+})
+export class AuroEditorComponent {
+
+}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { AuroEditorComponent } from './Component/auro-editor/auro-editor.component';
+
+export const routes: Routes = [
+    {path: '', component: AuroEditorComponent}
+];
